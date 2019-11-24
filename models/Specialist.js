bf6083fbe95c59158b26d344836b3ce48bdf4b0b
AtesAvci <56143069+AtesAvci@users.noreply.github.com>
2019-11-23 22:51:19 -0500

    edit
    edited patient, specialist, crated objectGen

--- models/Specialist.js
+++ models/Specialist.js
@@ -1,31 +1,17 @@
+
 const Schema = require("mongoose").Schema;
 const model = require("mongoose").model;
 
 const SpecSchema = new Schema({
     _id: Schema.Types.ObjectId,
-    name: {
-        first: {type: String, required: true},
-        last: {type: String, required: true}
-    },
+    name: {type: String, required: true},
     sex: {type: String, required: true},
+    email:{type: String, required: true},
     type: {type: String, required: true},
-    experience: {type: Date, required: true},
-    punctuality: {type: Number, required: true},
-    communication: {type: Number, required: true},
-    location: {type: String, required: true},
+    experience: {type: Number, required: true},
     availability: {type: Date, required: true},
-    reviews: [
-        {
-            rating: {type: Number, required: true},
-            comment: String
-        }
-    ],
-    appointments: [
-        {
-            _id: {type: String, required: true, unique: true},
-            date: {type: Date, required: true}
-        }
-    ]
+    distance: {type: String, required: true},
+    score: {type: Number, required: false}    
 });
 
 module.exports = model("specialist", SpecSchema);
