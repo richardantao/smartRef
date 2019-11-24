95be2dc7156cc36223522c380816f773deb5edd4
Richard Antao <richardmantao@gmail.com>
2019-11-24 06:07:34 -0500

    Updated component

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -27,11 +27,7 @@ export default class Search extends Component {
 								<Input name="specType" type="text" placeholder="Specialist Type"/>
 							</FormGroup>
 								<Button onSubmit={this.handleSubmit}>
-<<<<<<< HEAD
 									<FontAwesomeIcon icon={faSearch}/>
-=======
-									<FontAwesomeIcon/>
->>>>>>> 15322d109b548c1d2171c8d8536bb27b3d707e1a
 								</Button>
 						</Form>
 					</Col>
